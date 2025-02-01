20c4eccdc84b19840add68ee218c069fc2e3f52e
Krizzna69 <jaswanthuchiha69@gmail.com>
2025-02-01 17:52:49 +0530

    url

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=True)
