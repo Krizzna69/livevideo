1d005a79ad4add6a7632b8d48b708d3b72a26165
Krizzna69 <jaswanthuchiha69@gmail.com>
2025-02-01 17:55:31 +0530

    added gunicorn

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
-@app.route('/')
+@app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
